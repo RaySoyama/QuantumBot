bee32dfe69e9441b437819202a1231d1f84f0be4
Ray Soyama <raysoyama@gmail.com>
2019-02-01 00:35:12 -0800

    Literal Garbage Stalker Chat Recorder

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System;
 using System.Threading.Tasks;
 using Discord.WebSocket;
 using Discord;
+using System.Threading;
+using System.IO;
 
 namespace DiscordBot
 {
@@ -12,6 +14,8 @@ namespace DiscordBot
     {
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
+
+        DiscordSocketClient client;
 
         public async Task MainAsync()
         {
@@ -38,18 +42,98 @@ namespace DiscordBot
 
         private async Task MessageReceived(SocketMessage message)
         {
-            if (message.Content == "OwO")
+            bool isNewSentence;
+            string oldmsg = "";
+
+            var msg = message as SocketUserMessage; // cast to a USER msg;
+
+            if (msg.ToString() == oldmsg)
             {
-                await message.Channel.SendMessageAsync("UwU");
+                isNewSentence = false;
             }
-            if (message.Content == "!commands")
+            else
             {
-                await message.Channel.SendMessageAsync("Suck my robot dick, Carter");
+                oldmsg = msg.ToString();
+                isNewSentence = true;
             }
-            if (message.Content.Contains("Roasted"))
+
+            if (msg == null || msg.Author.ToString() == "Quantum Bot#5354") //if msg is from bot,
             {
-                await message.Channel.SendMessageAsync("I heard someone needed this https://bit.ly/2CZbNrL");
+                return;
             }
+
+            string path = "DiscordChatData.txt";
+            string chatLog = "Time: " + msg.Timestamp.ToString() + "\nChannel: " + msg.Channel.ToString() + "\nUsername: " + msg.Author.ToString() + "\nMessage: " + msg.ToString() + "\n\n";
+
+            File.AppendAllText(path, chatLog);
+            
+            
+
+            Console.WriteLine(chatLog);
+            int argPos = 0;
+            char prefix = '*';
+
+            if (LanguageFilter(msg.ToString().Substring(argPos)) == true && isNewSentence == true)
+            {
+                await message.DeleteAsync();
+                await message.Channel.SendMessageAsync("DONT FUCKING SWEAR IN MY FUCKING SERVER, REEEEEEEEEEE");
+               
+                await message.Channel.SendMessageAsync("jk, " + msg.Author.ToString() + " said  \"" + msg.ToString() + "\"");
+                return;
+            }
+
+            if (msg.ToString().ToLower().Contains("lol"))
+            {
+                await message.Channel.SendMessageAsync("NO LAUGHING IN THIS SERVER");
+            }
+            else if (msg.ToString().ToLower().Contains("!help"))
+            {
+                await message.Channel.SendMessageAsync("I ain't Dyno bot, who the fuck?");
+            }
+            else if (msg.ToString().ToLower() == "!quit" && msg.Author.ToString() == "Quantum Blue#1234")
+            {
+                await message.Channel.SendMessageAsync("I'll be back - Steve Jobs");
+                System.Environment.Exit(1);
+            }
+        }
+
+        public bool LanguageFilter(string sentence)
+        {
+            string temp = sentence.ToLower();
+
+            string[] BadWords =
+                {
+                "Fuck",
+                "Shit",
+                "Ass",
+                "Terry",
+                "UwU",
+                "OwO",
+                "Snuggles",
+                "Bulgy",
+                "o3o",
+                "Rawr",
+                "nuzzles",
+                "daddy",
+                "shaft",
+                "nyan",
+                "moans",
+                "musky",
+                "paws",
+                "rubbies",
+                "salty",
+                };
+
+            for(int i = 0; i < BadWords.Length; i++)
+            {
+                if (temp.Contains(BadWords[i].ToLower()) == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+
         }
 
     }
